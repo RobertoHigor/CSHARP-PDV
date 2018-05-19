33739a274ec375665a12881503f288c643a353c2
akronemberg <akronemberg@faeterj-petropolis.edu.br>
2018-05-18 21:15:08 -0300

    dsd

---
+++ SQLiteBD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WindowsFormsApp1
+{
+    class SQLiteBD
+    {
+        private const string STR_CONEXAO = "Data source=dados.db;Version=3";
+        public SQLiteBD()
+        {
+            if (System.IO.File.Exists("dados.db"))
+                CriarBD();
+        }
+
+        private void CriarBD()
+        {
+            SQLiteConnection conexao;
+        }
+    }
+}
